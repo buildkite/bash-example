d3ed427152b2edc5b975c54cb88c80449cddbd09
Keith Pitt <me@keithpitt.com>
2014-05-22 19:43:14 +0800

    First cut at the example script.

---
+++ example_script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# the flag tells bash to exit if a command fails
+set -e
+
+echo -e "\033[33m
+  _           _ _     _ _
+ | |         (_) |   | | |
+ | |__  _   _ _| | __| | |__   _____  __
+ | '_ \| | | | | |/ _\` | '_ \ / _ \ \/ /
+ | |_) | |_| | | | (_| | |_) | (_) >  <
+ |_.__/ \__,_|_|_|\__,_|_.__/ \___/_/\_\\033[0m"
+
+echo "\n\nYay, you've successfully ran a script on Buildbox!"
