45a9c8a47a45a435ef464943011270c40c233d16
Tim Lucas <t@toolmantim.com>
2016-02-17 19:23:38 +1100

    Update the example

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM ubuntu
-
-ADD . /app
-WORKDIR /app
--- script.sh
+++ script.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# This is a simple example bash script to get you started with Buildkite. If
-# this test succeeds (which it does) then your build will be green.
-#
-# To read more about what you can do in your build scripts, see the
-# Writing Build Scripts guide:
-# https://buildkite.com/docs/guides/writing-build-scripts
-
 set -eo pipefail
 
 echo "--- Build job checkout directory"
@@ -20,7 +13,7 @@ env
 
 echo "+++ Example tests"
 
-ASCII=`cat ascii/homer.txt`
+ASCII=$(cat ascii/thumbsup.txt)
 
 echo -e "\033[33mCongratulations!\033[0m You've successfully run your first build on Buildkite! 👍
 
