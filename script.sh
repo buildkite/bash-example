6116adb0eac0770f58b477f4e3563b1a6b90f536
Keith Pitt <me@keithpitt.com>
2019-12-13 15:02:18 +0800

    Another test

--- script.sh
+++ script.sh
@@ -10,7 +10,6 @@ echo "--- :evergreen_tree: Build job environment"
 
 env
 
-
 echo "+++ :hammer: Example tests 🦹"
 
 echo -e "\033[33mCongratulations!\033[0m You've successfully run your first build on Buildkite! 👍
