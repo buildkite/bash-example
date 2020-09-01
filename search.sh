34a07a5223915986fc2e49007b34ab0ec826ac79
Chloe Hutchinson <chloe@buildkite.com>
2020-09-02 09:18:18 +1000

    Test multiple artifacts of the same name

--- search.sh
+++ search.sh
@@ -1,8 +1,8 @@
 set -eo pipefail
 
 echo "+++ :mag: Searching for the artifacts we uploaded..."
-echo "> buildkite-agent artifact search \"*\""
-buildkite-agent artifact search "*"
+echo "> buildkite-agent artifact search \"artifacts/image.gif\""
+buildkite-agent artifact search "artifacts/image.gif"
 
 echo
 echo "For more info, visit our docs: https://buildkite.com/docs/pipelines/artifacts"
