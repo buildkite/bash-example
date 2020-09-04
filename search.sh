a378c730b8029d719c221b9625368a7b2d0f6f8e
Chloe Hutchinson <chloe@buildkite.com>
2020-09-04 14:12:25 +1000

    Improvements to output to test various conditions

--- search.sh
+++ search.sh
@@ -1,7 +1,16 @@
 set -eo pipefail
 
 echo "+++ :mag: Searching for the artifacts we uploaded..."
+echo "1. Search for * to find everything"
 buildkite-agent artifact search "*"
+
+echo "2. Search for artifacts/image.gif to test that we can handle duplicates"
+buildkite-agent artifact search "artifacts/image.gif"
+
+echo "3. Search for something that doesn't exist"
+set +e # the next command is expected to exit non-zero
+result=buildkite-agent artifact search "horses"
+echo "Artifact search returned $? $result"
 
 echo
 echo "For more info, visit our docs: https://buildkite.com/docs/pipelines/artifacts"
